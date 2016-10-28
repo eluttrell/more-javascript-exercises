bcbdbf19e2cfe0e6a04879d524f84936dc52e157
Elijah <elieel@gmail.com>
2016-10-28 13:09:22 -0400

    Move on to next exercises

--- caesar.js
+++ caesar.js
@@ -1,31 +1,4 @@
-// function cipher(text) {
-//   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-//   var result = '';
-//   for (var i = 0; i < text.length; i++) {
-//     var chr = text[i];
-//     var idx = alphabet.indexOf(chr.toUpperCase());
-//     var newIdx = idx + 13;
-//     if (newIdx >= alphabet.length) {
-//       newIdx -= 26;
-//     }
-//     result += alphabet[newIdx];
-//   }
-//   return result;
-// }
-
 // Caesar
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-// function cipher(str) {
-//   return
-// }
-// var x = 'hello';
-// x = x.split('');
-// function toNum(a) {
-//   return a.charCodeAt(0) + 13;
-// }
-// var done = x.map(toNum);
-// console.log(done);
-
 function toNum(a) {
   var x = a.charCodeAt(0) + 13;
   if (x > 122) {
@@ -39,3 +12,37 @@ var cipher = function (str) {
 };
 
 console.log(cipher('hello'));
+
+// =============================================================================
+
+// Toby's Caesar (NOT WORKING)
+// var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+//
+// function mapChar(chr) {
+//   var result = '';
+//   var idx = alphabet.indexOf(chr.toUpperCase());
+//   var newIdx = idx + 13;
+//   if (newIdx >= alphabet.length) {
+//     newIdx -= 26;
+//   }
+//   result += alphabet[newIdx];
+// }
+//
+// function cipher(text) {
+//   return text.split('').map(mapChar).join('');
+// }
+//
+// cipher('HELLO');
+// var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+// var result = '';
+// for (var i = 0; i < text.length; i++) {
+//   var chr = text[i];
+// var idx = alphabet.indexOf(chr.toUpperCase());
+// var newIdx = idx + 13;
+// if (newIdx >= alphabet.length) {
+//   newIdx -= 26;
+// }
+// result += alphabet[newIdx];
+// }
+// return result;
+// }
